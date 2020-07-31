29fe8190b1d6956fceb697e4e52be392e7dfd1ec
DavidPinilla92 <d.pinilla@geekshubs.com>
2020-07-31 13:22:48 +0200

    add signup route

--- routes/api.php
+++ routes/api.php
@@ -8,13 +8,15 @@ Route::middleware('auth:api')->get('/user', function (Request $request)
 {
     return $request->user();
 });
-
+Route::prefix('auth')->group(function(){
+    Route::post('signup','AuthController@signup');
+});
 Route::prefix('product')->group(function ()
 {
     Route::get('', 'ProductController@getAll');
     Route::post('', 'ProductController@create');
-    Route::put('/{id}', 'ProductController@update');
-    Route::delete('/{id}', 'ProductController@delete');
+    Route::put('{id}', 'ProductController@update');
+    Route::delete('{id}', 'ProductController@delete');
 });
 Route::prefix('category')->group(function ()
 {
